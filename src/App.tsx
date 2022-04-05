44ae931ac167a27ee34e0d61105f9769ce10049a
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-04-05 16:17:27 +0200

    Bootstrap tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
-import Tabs from "./components/tabs/Tabs";
+import TabsPage from "./components/tabs/TabsPage";
+import BSTabs from "./components/tabs/BSTabs";
 
 function App() {
   return (
     <div className="App">
-      <Tabs/>
+      <TabsPage/>
+      <BSTabs/>
     </div>
   );
 }
--- src/components/tabs/BSTabs.tsx
+++ src/components/tabs/BSTabs.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import {Tab, Tabs} from "react-bootstrap";
+import '../../App.css';
+
+export enum Page {
+    COMPANIES,
+    ACTIVITIES,
+    DECLARATIONS,
+}
 
 const BSTabs = () => {
+    const [key, setKey] = useState("declarations");
 
     return(
-        <>BS tabs</>
+        <div className="container">
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                defaultActiveKey={"declarations"}
+                onSelect={(k) => { if (k) setKey(k) }}
+                className="bloc-tabs"
+            >
+                <Tab
+                    eventKey="companies"
+                    title="Companies"
+                    tabClassName={key === 'companies' ? 'tabs active-tabs' : 'tabs'}
+                    className={key === 'companies' ? "content  active-content" : "content"}
+                >
+                    Content 1
+                </Tab>
+                <Tab
+                    eventKey="activities"
+                    title="Activities"
+                    tabClassName={key === 'activities' ? 'tabs active-tabs' : 'tabs'}
+                    className={key === 'activities' ? "content  active-content" : "content"}
+                >
+                    Content 2
+                </Tab>
+                <Tab
+                    eventKey="declarations"
+                    title="Declarations"
+                    tabClassName={key === 'declarations' ? 'tabs active-tabs' : 'tabs'}
+                    className={key === 'declarations' ? "content  active-content" : "content"}
+                >
+                    Content 3
+                </Tab>
+            </Tabs>
+        </div>
     )
 }
 
