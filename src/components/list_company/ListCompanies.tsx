b24a57adae9bcee6b97e4abae87e13d1bf5c87a4
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-06-20 09:45:15 +0200

    ListCompanies naming refactor

---
+++ src/components/list_company/ListCompanies.tsx
@@ -0,0 +1,7 @@
+import { Card } from "react-bootstrap";
+
+const ListCompanies = () => {
+  return <Card className={"pt-4"}>List companies</Card>;
+};
+
+export default ListCompanies;
--- src/components/tabs/BSTabs.tsx
+++ src/components/tabs/BSTabs.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
-import {Tab, Tabs} from "react-bootstrap";
-import '../../App.css';
+import { useState } from "react";
+import { Tab, Tabs } from "react-bootstrap";
+import "../../App.css";
+import ListCompanies from "../list_company/ListCompanies";
+import WorkUnitForm from "../WorkUnitForm/WorkUnitForm";
 
 /**
 export enum Page {
@@ -11,44 +13,52 @@ export enum Page {
 */
 
 const BSTabs = () => {
-    const [key, setKey] = useState("declarations");
+  const [key, setKey] = useState("declarations");
 
-    return(
-        <div className="container">
-            <Tabs
-                id="controlled-tab-example"
-                activeKey={key}
-                defaultActiveKey={"declarations"}
-                onSelect={(k) => { if (k) setKey(k) }}
-                className="bloc-tabs"
-            >
-                <Tab
-                    eventKey="companies"
-                    title="Companies"
-                    tabClassName={key === 'companies' ? 'tabs active-tabs' : 'tabs'}
-                    className={key === 'companies' ? "content  active-content" : "content"}
-                >
-                    Content 1
-                </Tab>
-                <Tab
-                    eventKey="activities"
-                    title="Activities"
-                    tabClassName={key === 'activities' ? 'tabs active-tabs' : 'tabs'}
-                    className={key === 'activities' ? "content  active-content" : "content"}
-                >
-                    Content 2
-                </Tab>
-                <Tab
-                    eventKey="declarations"
-                    title="Declarations"
-                    tabClassName={key === 'declarations' ? 'tabs active-tabs' : 'tabs'}
-                    className={key === 'declarations' ? "content  active-content" : "content"}
-                >
-                    Content 3
-                </Tab>
-            </Tabs>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        defaultActiveKey={"declarations"}
+        onSelect={(k) => {
+          if (k) setKey(k);
+        }}
+        className="bloc-tabs"
+      >
+        <Tab
+          eventKey="companies"
+          title="Companies"
+          tabClassName={key === "companies" ? "tabs active-tabs" : "tabs"}
+          className={
+            key === "companies" ? "content  active-content" : "content"
+          }
+        >
+          <ListCompanies />
+        </Tab>
+        <Tab
+          eventKey="activities"
+          title="Activities"
+          tabClassName={key === "activities" ? "tabs active-tabs" : "tabs"}
+          className={
+            key === "activities" ? "content  active-content" : "content"
+          }
+        >
+          Content 2
+        </Tab>
+        <Tab
+          eventKey="declarations"
+          title="Declarations"
+          tabClassName={key === "declarations" ? "tabs active-tabs" : "tabs"}
+          className={
+            key === "declarations" ? "content  active-content" : "content"
+          }
+        >
+          <WorkUnitForm />
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
 
 export default BSTabs;
