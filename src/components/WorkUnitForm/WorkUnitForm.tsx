3f5ba1f6d9523c548799968e393f3ea82bd65c24
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-06-20 10:07:42 +0200

    Changeable first input

--- src/components/WorkUnitForm/WorkUnitForm.tsx
+++ src/components/WorkUnitForm/WorkUnitForm.tsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
+interface Person {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
 const WorkUnitForm = () => {
-  return <div>WorkUnitForm</div>;
+  const [people, setPeople] = useState<Person[]>([
+    { id: "5", firstName: "one", lastName: "two" },
+  ]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {people.map((p) => {
+        return (
+          <div key={p.id}>
+            <input
+              value={p.firstName}
+              placeholder={"first name"}
+              onChange={(e) => {
+                const firstName = e.target.value; // make sure that value is correct, since further it's closure
+                setPeople((currentPerson) =>
+                  currentPerson.map((x) =>
+                    x.id === p.id
+                      ? {
+                          ...x,
+                          firstName,
+                        }
+                      : x
+                  )
+                );
+                //e.target.value
+              }}
+            />
+            <input value={p.lastName} placeholder={"last name"} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default WorkUnitForm;
