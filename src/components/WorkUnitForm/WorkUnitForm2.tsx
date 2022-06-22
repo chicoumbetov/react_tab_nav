8de0a301ef2009654383e491748d59eb23d6aa41
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-06-22 12:13:10 +0200

    :construction: onChange, send button

--- src/components/WorkUnitForm/WorkUnitForm2.tsx
+++ src/components/WorkUnitForm/WorkUnitForm2.tsx
@@ -1,6 +1,6 @@
-import { Remove } from "@mui/icons-material";
+import { Remove, SendOutlined } from "@mui/icons-material";
 import AddIcon from "@mui/icons-material/Add";
-import { IconButton, TextField } from "@mui/material";
+import { Button, IconButton, TextField } from "@mui/material";
 import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { createUseStyles } from "react-jss";
@@ -9,16 +9,37 @@ const useStyles = createUseStyles({
   root: {
     "& .MuiFilledInput-root": {
       marginLeft: "10px",
+      marginTop: "10px",
     },
   },
+  button: {
+    marginTop: "10px",
+    marginLeft: "10px",
+  },
 });
+
+export interface InputFieldsType {
+  firstName: string;
+  lastName: string;
+}
 
 const WorkUnitForm2 = () => {
   const classes = useStyles();
 
   const [inputFields, setInputFields] = useState([
     { firstName: "", lastName: "" },
+    { firstName: "", lastName: "" },
   ]);
+
+  const handleChange = (
+    index: keyof InputFieldsType | number,
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const values = [...inputFields];
+    values[index as number][event.target.name as keyof InputFieldsType] =
+      event.target.value;
+    setInputFields(values);
+  };
 
   return (
     <Container className="m-2">
@@ -31,12 +52,14 @@ const WorkUnitForm2 = () => {
               label="First Name"
               variant="filled"
               value={inputField.firstName}
+              onChange={(event) => handleChange(index, event)}
             />
             <TextField
               name="lastName"
               label="First Name"
               variant="filled"
-              value={inputField.firstName}
+              value={inputField.lastName}
+              onChange={(event) => handleChange(index, event)}
             />
             <IconButton>
               <Remove />
@@ -46,6 +69,15 @@ const WorkUnitForm2 = () => {
             </IconButton>
           </div>
         ))}
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          endIcon={<SendOutlined />}
+          className={classes.button}
+        >
+          Send
+        </Button>
       </form>
     </Container>
   );
